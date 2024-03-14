4e6bf4725e280011576bd082959a3d0b27555068
arco_notebook <3481554467@qq.com>
2024-03-14 15:15:22 +0800

    utils:获取进程id/桶与时间id/迁移锁id/过滤器id/进程与时间组合id

---
+++ common/utils/timer.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"gotimer/common/consts"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// unix 是一个代表时间的 64 位整数，通常是自 1970 年 1 月 1 日 00:00:00 UTC 起的秒数
+func UnionTimerIDUnix(timeID uint, unix int64) string { return fmt.Sprintf("%d_%d", timeID, unix) }
+
+func SplitTimerIDUnix(str string) (uint, int64, error) {
+	timerIDUnix := strings.Split(str, "_")
+	if len(timerIDUnix) != 2 {
+		return 0, 0, fmt.Errorf("invalid timerID unix str: %s", str)
+	}
+
+	timerID, _ := strconv.ParseInt(timerIDUnix[0], 10, 64)
+	unix, _ := strconv.ParseInt(timerIDUnix[1], 10, 64)
+	return uint(timerID), unix, nil
+}
+
+func GetTaskBloomFilterKey(timerStr string) string { return "task_bloom_" + timerStr }
+
+func GetBucketCntKey(key string) string {
+	return "bucket_cnt_" + key
+}
+
+func GetTimeBucketLockKey(t time.Time, bucketID int) string {
+	return fmt.Sprintf("time_bucket_lock_%s_%d", t.Format(consts.MinuteFormat), bucketID)
+}
+func GetMigratorLockKey(t time.Time) string {
+	return fmt.Sprintf("migrator_lock_%s", t.Format(consts.HourFormat))
+}
+
+func GetMonitorLockKey(t time.Time) string {
+	return fmt.Sprintf("monitor_lock_%s", t.Format(consts.MinuteFormat))
+}
+
+func GetSliceMsgKey(t time.Time, bucketID int) string {
+	return fmt.Sprintf("%s_%d", t.Format(consts.MinuteFormat), bucketID)
+}
+
+func GetEnableLockKey(app string) string { return fmt.Sprintf("enable_timer_lock_%s", app) }
+
+func GetCreateLockKey(app string) string { return fmt.Sprintf("create_timer_lock_%s", app) }
+
+func GetForwardTwoMigrateStepEnd(cur time.Time, diff time.Duration) time.Time {
+	end := cur.Add(diff)
+	return time.Date(end.Year(), end.Month(), end.Day(), end.Hour(), end.Minute(), 0, 0, time.Local)
+}
--- dao/task/cache.go
+++ dao/task/cache.go
@@ -3,7 +3,9 @@ package task
 import (
 	"context"
 	"gotimer/common/conf"
+	"gotimer/common/model/po"
 	"gotimer/pkg/redis"
+	"time"
 )
 
 type TaskCache struct {
@@ -12,5 +14,25 @@ type TaskCache struct {
 }
 
 type cacheClient interface {
-	Transaction(ctx context.Context, commands ...*redis.Command) ([]interface{}, err)
+	Transaction(ctx context.Context, commands ...*redis.Command) ([]interface{}, error)
+	ZrangeByScore(ctx context.Context, table string, score1, score2 int64) ([]string, error)
+	Expire(ctx context.Context, key string, expireSeconds int64) error
+	MGet(ctx context.Context, keys ...interface{}) ([]string, error)
+}
+
+func NewTaskCache(client cacheClient, confProvider *conf.SchedulerAppConfProvider) *TaskCache {
+	return &TaskCache{
+		client:       client,
+		confProvider: confProvider,
+	}
+}
+
+func (t *TaskCache) BatchCreateBucket(ctx context.Context, cntByMins []*po.MinuteTaskCnt, end time.Time) error {
+	conf := t.confProvider.Get()
+
+	expireSeconds := int64(time.Until(end) / time.Second)
+	commands := make([]*redis.Command, 0, 2*len(cntByMins))
+	for _, detail := range cntByMins {
+		commands = append(commands, redis.NewSetCommand(utils.GetBu))
+	}
 }
